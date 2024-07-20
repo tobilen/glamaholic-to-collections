2a1ebe901965650f542a674c4ae5918d3d176e3d
Tobias Lengsholz <tobias.lengsholz@invision.de>
2024-07-20 06:31:45 +0200

    Fix path separators

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { writeCollectionsConfig } from "./lib.js";
 import * as fs from "node:fs";
+import * as path from "node:path";
 
 const argv = yargs(hideBin(process.argv))
   .usage(
@@ -13,21 +14,36 @@ const argv = yargs(hideBin(process.argv))
     describe: "Path to your existing collections.json file",
     type: "string",
     demandOption: false,
-    default: `${process.env.APPDATA}/XIVLauncher/pluginConfigs/Collections.json`,
+    default: path.join(
+      process.env.APPDATA,
+      "XIVLauncher",
+      "pluginConfigs",
+      "Collections.json",
+    ),
   })
   .option("glamaholicConfig", {
     alias: "gc",
     describe: "Path to your glamaholic.json file",
     type: "string",
     demandOption: false,
-    default: `${process.env.APPDATA}/XIVLauncher/pluginConfigs/Glamaholic.json`,
+    default: path.join(
+      process.env.APPDATA,
+      "XIVLauncher",
+      "pluginConfigs",
+      "Glamaholic.json",
+    ),
   })
   .option("outputFile", {
     alias: "o",
     describe: "Path to where you want to save the new collections.json file",
     type: "string",
     demandOption: false,
-    default: `${process.env.APPDATA}/XIVLauncher/pluginConfigs/Collections.json`,
+    default: path.join(
+      process.env.APPDATA,
+      "XIVLauncher",
+      "pluginConfigs",
+      "Collections.json",
+    ),
   })
   .help().argv;
 
